84dd35429bd950644f5f167ab082789afda97024
Jevgenij <jevgenijguzikovskij@coherentsolutions.com>
2022-08-18 17:16:08 +0300

    Restructured code

--- learningAngular/src/app/modules/appointment/app.module.ts
+++ learningAngular/src/app/modules/appointment/app.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { RouterModule, Routes } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { PageOneComponent } from './page-one/page-one.component';
-import { PageTwoComponent } from './page-two/page-two.component';
+import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
@@ -14,29 +10,23 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { AppRoutingModule } from './app-routing.module';
 
-const routes: Routes = [
-  { path:'page/1', component: PageOneComponent},
-  { path:'page/2', component: PageTwoComponent},
-
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageOneComponent,
-    PageTwoComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     MatButtonModule,
     MatDividerModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
     MatSliderModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
