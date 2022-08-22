0cf433606a270cd7b557fa21f5d7578485fed73b
Jevgenij <jevgenijguzikovskij@coherentsolutions.com>
2022-08-22 10:50:02 +0300

    Added car module, component and filled it

--- learningAngular/src/app/modules/user/components/user-page/user-page.component.ts
+++ learningAngular/src/app/modules/user/components/user-page/user-page.component.ts
@@ -1,11 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+interface User {
+  name: String,
+  age: Number,
+  isActive: boolean
+}
+
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss']
+  styleUrls: ['./user-page.component.scss', '../../../../styles/styles.scss', ]
 })
 export class UserPageComponent implements OnInit {
+  users: User[] = [
+    { name: 'Joe', age: 23, isActive: true },
+    { name: 'Ann', age: 12, isActive: false },
+    { name: 'Michael', age: 45, isActive: false },
+    { name: 'Natan', age: 31, isActive: true },
+    { name: 'Hugho', age: 21, isActive: false },
+    { name: 'Joe', age: 23, isActive: true },
+    { name: 'Ann', age: 12, isActive: false },
+    { name: 'Michael', age: 45, isActive: false },
+    { name: 'Natan', age: 31, isActive: true },
+    { name: 'Hugho', age: 21, isActive: false },
+  ]
+
+
+  buttonState: Boolean = false;
+
+  toggleButton() {
+    this.buttonState = !this.buttonState;
+    console.log('toggled');
+    
+  }
+
+  toggleUserStatus(user: User) {
+    let userIndex = this.users.indexOf(user);
+    this.users[userIndex].isActive = !this.users[userIndex].isActive;
+    console.log(this.users);
+    
+  }
 
   constructor() { }
 
--- learningAngular/src/app/modules/user/user.module.ts
+++ learningAngular/src/app/modules/user/user.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserPageComponent } from './components/user-page/user-page.component';
 
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     UserPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class UserModule { }
