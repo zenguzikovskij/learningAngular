92131a9434d88dd7592de10913ab49302e35da9c
Jevgenij <jevgenijguzikovskij@coherentsolutions.com>
2022-08-22 10:49:40 +0300

    Created car module, component and filled them

--- learningAngular/src/app/modules/car/car.module.ts
+++ learningAngular/src/app/modules/car/car.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarPageComponent } from './components/car-page/car-page.component';
 
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     CarPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    MatButtonModule
   ]
 })
 export class CarModule { }
--- learningAngular/src/app/modules/car/components/car-page/car-page.component.ts
+++ learningAngular/src/app/modules/car/components/car-page/car-page.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-car-page',
   templateUrl: './car-page.component.html',
-  styleUrls: ['./car-page.component.scss']
+  styleUrls: ['./car-page.component.scss', '../../../../styles/styles.scss']
 })
 export class CarPageComponent implements OnInit {
+  value: string = '';
 
   constructor() { }
 
